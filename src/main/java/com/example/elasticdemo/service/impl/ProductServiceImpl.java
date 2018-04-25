fe3c73864d077ed76c7c1b2a9cde463aacaaf70d
luica13 <luiza.kharatyan@gmail.com>
2018-04-26 00:38:55 +0400

    Update ProductServiceImpl.java
    added implementation

--- src/main/java/com/example/elasticdemo/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/elasticdemo/service/impl/ProductServiceImpl.java
@@ -1,41 +1,41 @@
 package com.example.elasticdemo.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.example.elasticdemo.entity.Product;
+import com.example.elasticdemo.repository.ProductRepository;
 import com.example.elasticdemo.service.ProductService;
 
 public class ProductServiceImpl implements ProductService {
+	   
+	@Autowired
+    private ProductRepository productRepository;
 
 	@Override
-	public Product save(Product Product) {
-		// TODO Auto-generated method stub
-		return null;
+	public Product save(Product product) {
+		return productRepository.save(product);
 	}
 
 	@Override
 	public Iterable<Product> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return productRepository.findAll();
 	}
 
 	@Override
 	public Page<Product> findByProductName(String name, Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
+		return productRepository.findByProductName(name, pageable);
 	}
 
 	@Override
 	public Page<Product> findByFilteredTagQuery(String tag, Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
+		return productRepository.findByFilteredTagQuery(category, pageable);
 	}
 
 	@Override
 	public long count() {
-		// TODO Auto-generated method stub
-		return 0;
+		return productRepository.count();
 	}
 
 }
